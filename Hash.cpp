9069b1bbb782c81279f5c32346612d1bbc455026
laith-nobani <160516728+laith-nobani@users.noreply.github.com>
2025-09-13 22:04:19 +0300

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -1,60 +1,50 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-
-// Multibase parameters
 const int BASE1 = 31, MOD1 = 1e9 + 7;
 const int BASE2 = 37, MOD2 = 1e9 + 9;
-
-// Fast power
-ll fastpow(ll num, ll p, ll MOD) {
-    if (p == 0) return 1;
-    if (p % 2) return (num % MOD * fastpow(num, p - 1, MOD)) % MOD;
-    ll a = fastpow(num, p / 2, MOD);
-    return (a * a) % MOD;
+vector<ll> powBase1, powBase2;
+void precomputePowers(int n) {
+    powBase1.resize(n+1);
+    powBase2.resize(n+1);
+    powBase1[0] = powBase2[0] = 1;
+    for(int i=1;i<=n;i++){
+        powBase1[i] = (powBase1[i-1] * BASE1) % MOD1;
+        powBase2[i] = (powBase2[i-1] * BASE2) % MOD2;
+    }
 }
-
-// Hash operations using pair for multibase
+int val(char c) { return c - 'a' + 1; }
 pair<ll,ll> shiftLeft(pair<ll,ll> code) {
     return {(code.first * BASE1) % MOD1, (code.second * BASE2) % MOD2};
 }
 
 pair<ll,ll> addAt(pair<ll,ll> code, int idx, int v) {
-    return {(code.first + v * fastpow(BASE1, idx, MOD1)) % MOD1,
-            (code.second + v * fastpow(BASE2, idx, MOD2)) % MOD2};
+    return {(code.first + v * powBase1[idx]) % MOD1,
+            (code.second + v * powBase2[idx]) % MOD2};
 }
 
 pair<ll,ll> removeAt(pair<ll,ll> code, int idx, int v) {
-    return {(code.first - v * fastpow(BASE1, idx, MOD1) + MOD1) % MOD1,
-            (code.second - v * fastpow(BASE2, idx, MOD2) + MOD2) % MOD2};
+    return {(code.first - v * powBase1[idx] + MOD1) % MOD1,
+            (code.second - v * powBase2[idx] + MOD2) % MOD2};
 }
 
-// Character value
-int val(char c) { return c - 'a' + 1; }
-
-// Pattern search using multibase
-void pattern_search(string main, string pat) {
+ll pattern_search(const string &main, const string &pat) {
     int n = pat.size();
     pair<ll,ll> patCode = {0,0};
     for(int i=0;i<n;i++){
         patCode = shiftLeft(patCode);
-        patCode = addAt(patCode, 0, val(pat[i]));
+        patCode = addAt(patCode,0,val(pat[i]));
     }
 
     pair<ll,ll> subCode = {0,0};
+    ll ans = 0;
     for(int i=0;i<(int)main.size();i++){
         if(i - n >= 0) subCode = removeAt(subCode, n-1, val(main[i-n]));
         subCode = shiftLeft(subCode);
-        subCode = addAt(subCode, 0, val(main[i]));
-        if(subCode == patCode){
-            cout << "Found match at index " << i-n+1 << "!\n";
-        }
+        subCode = addAt(subCode,0,val(main[i]));
+        if(subCode == patCode) ans++;
     }
+    return ans;
 }
 
-// Longest suffix palindrome using multibase
-int longestSuffixPalindrome(string str) {
+int longestSuffixPalindrome(string &str) {
     int n = str.size();
     pair<ll,ll> code = {0,0};
     pair<ll,ll> codeRev = {0,0};
@@ -73,3 +63,10 @@ int longestSuffixPalindrome(string str) {
 }
 
 
+void solve(){
+    string st1, st2;
+    cin >> st1 >> st2;
+    precomputePowers(max(st1.size(), st2.size()) + 5);
+    ll count = pattern_search(st1, st2);
+    cout << count << "\n";
+}
